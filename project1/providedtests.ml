f6279709fad77bc28c971d8ff88b8df0ac78b7a4
Andrew Braunstein <awbraunstein@gmail.com>
2011-01-31 22:13:50 +0000

    working on user tests

--- project1/providedtests.ml
+++ project1/providedtests.ml
@@ -1,5 +1,7 @@
 open Assert
 open Interpreter
+open X86
+open Gradedtests
 
 (* These tests are provided by you -- they will be graded manually *)
 
@@ -7,7 +9,30 @@ open Interpreter
 (* debug your program.                                          *)
 
 let provided_tests : suite = [
-  Test ("Student-Provided Big Test for Part II", [
-  ]);
-  
-] 
+  Test("Control-Flow", 
+   [("test-call", run_test 7l
+    [(mk_insn_block (mk_lbl_named "call") [
+      Sub (eax, Imm 1l);
+      Ret
+    ]); 
+     (mk_insn_block (mk_lbl_named "main") [
+      Add (eax, Imm 4l);
+      Call (Lbl (mk_lbl_named "j"));
+      Add (eax, Imm 4l);
+      Ret
+    ]);]
+   );
+  ("test-j", run_test 7l
+    [(mk_insn_block (mk_lbl_named "j") [
+      Sub (eax, Imm 1l);
+      Ret
+    ]); 
+     (mk_insn_block (mk_lbl_named "main") [
+      Add (eax, Imm 4l);
+      Call (Lbl (mk_lbl_named "j"));
+      Add (eax, Imm 4l);
+      Ret
+    ]);]
+   )
+  ])
+]
