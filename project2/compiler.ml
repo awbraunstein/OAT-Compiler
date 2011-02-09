bb098622e0b50e71c869595ceff44cf88f93a22b
Nico Mihalich <nmihalich@gmail.com>
2011-02-09 21:51:58 +0000

    mult-a-bitch

--- project2/compiler.ml
+++ project2/compiler.ml
@@ -28,16 +28,15 @@ let parse (filename : string) (buf : Lexing.lexbuf) : exp =
    Follows cdecl calling conventions and platform-specific name mangling policy. *)
 let (>::) x y =y::x  
 
-let test1 : insn list = [Mov (edx, stack_offset (4l))] @ [Mov (eax, edx)] @ [Ret]
-
   
 let rec emit_exp (exp:exp) (stream : insn list) : insn list =
   begin match exp with
     | Cint i -> stream >:: Mov (eax, Imm i)
     | Arg -> Mov (eax, edx)::stream
     (*| Binop (a, l, r) -> binop_aux a l r stream*)
-    (*| Unop (a, x) -> unop_aux a x stream*)
+    | Unop (a, x) -> stream
   end
+
 (*  
 and unop_aux (u:unop) (x:exp) (i:insn list): insn list=
   begin match u with
@@ -46,11 +45,7 @@ and unop_aux (u:unop) (x:exp) (i:insn list): insn list=
     | Neg -> X86.Neg(eax)::emit_exp x [] @i
   end
   *)
-(*  
-and binop_aux2(x:exp)(y:exp)(i:insn list):insn list=
-  emit_exp y []@Mov(ecx, eax)::emit_exp x []@i
-  *)
-(*  
+
 and binop_aux (b:binop) (l:exp) (r:exp) (i: insn list): insn list=
   begin match b with
     | Plus ->
@@ -60,14 +55,31 @@ and binop_aux (b:binop) (l:exp) (r:exp) (i: insn list): insn list=
       (emit_exp r str_l) >::
       (Add(eax, stack_offset (4l))) >::
       (Add(esp, Imm 4l))
+    | Times ->
+      let str_l = 
+        (emit_exp l i) >:: (Push eax)
+      in
+      (emit_exp r str_l) >::
+      (Imul(Eax, stack_offset (4l))) >::
+      (Add(esp, Imm 4l))
+    | Minus -> i
+    | Ast.Eq -> i(* binary equality *)
+    | Neq -> i(* binary inequality *)
+    | Lt -> i(* binary signed less-than *)
+    | Lte -> i(* binary signed less-than or equals *)
+    | Gt -> i(* binary signed greater-than *)
+    | Gte -> i(* binary signed greater-than or equals *)
+    | Ast.And -> i(* binary bitwise and *)
+    | Ast.Or -> i(* binary bitwise or *)
+    | Ast.Shl -> i(* binary shift left *)
+    | Ast.Shr -> i(* binary logical shift right *)
+    | Ast.Sar -> i(* binary arithmetic shift right *)
   end
-*)
+  
 let compile_exp (ast:exp) : Cunit.cunit =
   let block_name = (Platform.decorate_cdecl "program") in
     let init_str = [Mov (edx, stack_offset (4l))] in
-     (* let insns = List.rev(emit_exp ast init_str >:: X86.Ret)  in*)
-    let insns = test1 in
+    let insns = List.rev(emit_exp ast init_str >:: X86.Ret)  in
         let block : X86.insn_block = {global = true; label = (mk_lbl_named block_name); insns=insns} in
           let comp = Cunit.Code block in
             [comp]
-      
