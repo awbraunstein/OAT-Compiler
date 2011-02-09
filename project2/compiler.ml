a34f0354a4bdd55059939fdf33c76c84905a6789
Andrew Braunstein <awbraunstein@gmail.com>
2011-02-09 02:05:03 +0000

    trying to build Cunit.cunit

--- project2/compiler.ml
+++ project2/compiler.ml
@@ -35,15 +35,16 @@ let rec emit_exp (exp:exp) (stream : insn list) : insn list =
     | Cint i -> Mov (eax, Imm i) :: stream
     | Arg -> Mov (eax, edx) :: stream
     | Binop (a, x, y) -> binop_aux a x y stream
-    | Unop (a, x) -> unop_aux a x stream
+    (*| Unop (a, x) -> unop_aux a x stream*)
   end
-  
+(*  
 and unop_aux (u:unop) (x:exp) (i:insn list): insn list=
   begin match u with
     | Not -> X86.Not(eax)::emit_exp x []@i
     | Lognot -> X86.Not(eax)::emit_exp x []@i
     | Neg -> X86.Neg(eax)::emit_exp x [] @i
   end
+  *)
   
 and binop_aux2(x:exp)(y:exp)(i:insn list):insn list=
   emit_exp y []@Mov(ecx, eax)::emit_exp x []@i
@@ -52,17 +53,21 @@ and binop_aux (b:binop) (x:exp) (y:exp) (i: insn list): insn list=
   begin match b with
     | Plus -> X86.Add(eax, ecx)::binop_aux2 x y i
     | Minus -> X86.Sub(eax, ecx)::binop_aux2 x y i
-    | Times -> X86.Imul(eax, ecx)::binop_aux2 x y i
-    | And -> X86.And(eax, ecx)::binop_aux2 x y i
-    | Or  -> X86.Or(eax,ecx)::binop_aux2 x y i
-    | Shl -> X86.Shl(eax,ecx)::binop_aux2 x y i
-    | Shr -> X86.Shr(eax,ecx)::binop_aux2 x y i
-    | Sar -> X86.Sar(eax,ecx)::binop_aux2 x y i
+    | Times -> X86.Imul(Eax, ecx)::binop_aux2 x y i
+    | Ast.And -> X86.And(eax, ecx)::binop_aux2 x y i
+    | Ast.Or  -> X86.Or(eax,ecx)::binop_aux2 x y i
+    | Ast.Shl -> X86.Shl(eax,ecx)::binop_aux2 x y i
+    | Ast.Shr -> X86.Shr(eax,ecx)::binop_aux2 x y i
+    | Ast.Sar -> X86.Sar(eax,ecx)::binop_aux2 x y i
   end
 
-and compile_exp (ast:exp) : Cunit.cunit =
+let compile_exp (ast:exp) : Cunit.cunit =
   let block_name = (Platform.decorate_cdecl "program") in
     Mov (edx, stack_offset 4l);
       let stream = [] in
-        let c = emit_exp ast [];
+        let c = emit_exp ast stream in
+          let block : X86.insn_block = X86.mk_block block_name c in
+            let comp : Cunit.component = block in
+              let unit : Cunit.cunit = [comp] in 
+                unit;
       
