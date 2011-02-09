d46648910522c177eb1ea78bed8eaaa11b5ecc47
Andrew Braunstein <awbraunstein@gmail.com>
2011-02-09 21:29:57 +0000

    building insnblock

--- project2/compiler.ml
+++ project2/compiler.ml
@@ -28,13 +28,14 @@ let parse (filename : string) (buf : Lexing.lexbuf) : exp =
    Follows cdecl calling conventions and platform-specific name mangling policy. *)
 let (>::) x y =y::x  
 
+let test1 : insn list = [Mov (edx, stack_offset (4l))] @ [Mov (eax, edx)] @ [Ret]
 
   
 let rec emit_exp (exp:exp) (stream : insn list) : insn list =
   begin match exp with
     | Cint i -> stream >:: Mov (eax, Imm i)
-    | Arg -> stream >:: Mov (eax, edx)
-    | Binop (a, l, r) -> binop_aux a l r stream
+    | Arg -> Mov (eax, edx)::stream
+    (*| Binop (a, l, r) -> binop_aux a l r stream*)
     (*| Unop (a, x) -> unop_aux a x stream*)
   end
 (*  
@@ -45,10 +46,11 @@ and unop_aux (u:unop) (x:exp) (i:insn list): insn list=
     | Neg -> X86.Neg(eax)::emit_exp x [] @i
   end
   *)
-  
+(*  
 and binop_aux2(x:exp)(y:exp)(i:insn list):insn list=
   emit_exp y []@Mov(ecx, eax)::emit_exp x []@i
-  
+  *)
+(*  
 and binop_aux (b:binop) (l:exp) (r:exp) (i: insn list): insn list=
   begin match b with
     | Plus ->
@@ -59,13 +61,13 @@ and binop_aux (b:binop) (l:exp) (r:exp) (i: insn list): insn list=
       (Add(eax, stack_offset (4l))) >::
       (Add(esp, Imm 4l))
   end
-
+*)
 let compile_exp (ast:exp) : Cunit.cunit =
   let block_name = (Platform.decorate_cdecl "program") in
     let init_str = [Mov (edx, stack_offset (4l))] in
-      let insns = List.rev(emit_exp ast init_str) >:: X86.Ret in
-          let block : X86.insn_block = X86.mk_block block_name insns in
-            let comp = Cunit.Code block in
-              let unit : Cunit.cunit = [comp] in 
-                unit;
+     (* let insns = List.rev(emit_exp ast init_str >:: X86.Ret)  in*)
+    let insns = test1 in
+        let block : X86.insn_block = {global = true; label = (mk_lbl_named block_name); insns} in
+          let comp = Cunit.Code block in
+            [comp]
       
