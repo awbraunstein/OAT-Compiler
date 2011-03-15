06219a389c31a82fd2afb67257cf7471f3e3c2c7
Nico Mihalich <nmihalich@gmail.com>
2011-03-15 21:30:24 +0000

    YAY PROGRESS!

--- project4/tc.ml
+++ project4/tc.ml
@@ -8,20 +8,93 @@ let report_error (info:Range.t) (expected:typ) (t:typ) : string =
     "%s: This expression has type %s but an expression was expected of type %s." 
     (Range.string_of_range info) (string_of_typ expected) (string_of_typ t)
 
+let rec tc_typ y z c : typ =
+  if (tc_exp y c <> tc_exp z c) then
+          failwith (report_error (exp_info y) (tc_exp y c) (tc_exp z c)) else TBool
 
+and tc_bool y z c : typ = 
+  if (tc_exp y c <> TBool) then
+          failwith (report_error (exp_info y) TBool (tc_exp y c))
+           else if (tc_exp z c <> TBool) then
+          failwith (report_error (exp_info z) TBool (tc_exp z c))
+          else TBool
+
+and tc_int y z c : typ = 
+  if (tc_exp y c <> TInt) then
+          failwith (report_error (exp_info y) TInt (tc_exp y c))
+         else if (tc_exp z c <> TInt) then
+          failwith (report_error (exp_info z) TInt (tc_exp z c))
+          else TInt
+          
+and tc_binop x y z (c:ctxt) : typ =
+  begin match x with
+        | Eq (_) -> tc_typ y z c
+        | Neq (_) -> tc_typ y z c
+        | And (_) -> tc_bool y z c
+        | Or (_) -> tc_bool y z c
+        | (_) -> tc_int y z c
+  end
+
+and tc_const x (c:ctxt) : typ =
+  begin match x with
+    | Cbool _ -> TBool
+    | Cint _ -> TInt
+    | Cstring _ -> TString
+  end
+
+and tc_unop x y c : typ =
+  begin match x with
+    | Neg (_) -> if (tc_exp y c <> TInt) then failwith "" else TInt
+    | Not (_) -> if (tc_exp y c <> TInt) then failwith "" else TInt
+    | Lognot (_) -> if (tc_exp y c <> TBool) then failwith "" else TBool
+  end
+    
+and tc_exp (e:Range.t exp) (c:ctxt) : typ =
+ begin match e with
+    | Binop (x,y,z) -> tc_binop x y z c
+    | Const (x) -> tc_const x c
+    | Lhs (x) -> failwith ""
+    | New (x,y,z) -> failwith ""
+    | Unop (x,y) -> failwith ""
+    | Ecall (x,y) -> failwith ""
+  end
+
+let typecheck_fdecl (p:Range.t prog) (c:ctxt) : unit =
+  begin match p with
+    | _ -> failwith "error"
+  end
+
+let typecheck_vdecl (p:Range.t prog) (c:ctxt) : unit =
+  begin match p with
+    | _ -> failwith "error"
+  end
+
+let get_decls (p:Range.t prog) : ctxt =
+  let c = enter_scope empty_ctxt in
+    let rec typecheck_h (c:ctxt) (h:Range.t Ast.gdecl) : ctxt =
+      begin match h with
+		      | Gvdecl v  ->
+		        begin match v with
+		          | {v_ty = x; v_id = y; v_init = z;} ->
+		            begin match y with
+		              | (_,a) -> add_vdecl a x c
+		            end
+		        end
+	       | Gfdecl f ->
+           begin match f with
+		         | (rtyp, id, args, block, exp) -> failwith ""
+		       end
+         | _ -> failwith "no program"
+		   end in
+      List.fold_left typecheck_h c p
+  
 
 let typecheck_prog (p:Range.t prog) : unit =
-  begin match p with
-    | h::tl ->
+  let c = get_decls p in
+      let rec typecheck_h (h:Range.t Ast.gdecl) : unit =
       begin match h with
-        | Gvdecl v  ->
-          begin match v with
-            | _ -> failwith "noprogram"
-          end
-        | Gfdecl f ->
-          begin match f with
-            | (rtyp,id, args, block,exp) -> failwith "no program"
-          end
-      end
-    | _ -> failwith "no program"
-  end
+		     | Gvdecl v  -> failwith ""
+	       | Gfdecl f -> failwith ""
+         | _ -> failwith "no program"
+		   end in
+      List.iter typecheck_h p
